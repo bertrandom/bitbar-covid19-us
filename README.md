920378af9175da32d043c5c1dc620886c6edffb5
Bertrand Fan <bert@slack-corp.com>
2020-03-12 16:50:58 -0700

    Pull data from git instead of the API

--- README.md
+++ README.md
@@ -8,5 +8,16 @@ BitBar plugin to show Coronavirus COVID-19 cases in the U.S. in your menubar, re
 ## Usage
 
 * Install [BitBar](https://github.com/matryer/bitbar)
-* Install `jq` (`brew install jq`)
+* Install `xsv` (`brew install xsv`)
+
+Clone the COVID-19 Case data somewhere, e.g.
+
+```
+cd ~/code
+git clone git@github.com:CSSEGISandData/COVID-19.git
+```
+
 * Copy `covid19.1h.sh` to your BitBar plugins directory
+
+Change the `COVID_DATA_DIR` at the top of `covid19.1h.sh` to point to the COVID-19 data directory, e.g.
+`COVID_DATA_DIR="/Users/bert/code/COVID-19/"`
--- covid19.1h.sh
+++ covid19.1h.sh
@@ -7,10 +7,19 @@
 # <bitbar.image>https://github.com/bertrandom/bitbar-covid19-us/blob/master/screenshot.png</bitbar.image>
 # <bitbar.dependencies>jq,tr</bitbar.dependencies>
 # <bitbar.abouturl>https://github.com/bertrandom/bitbar-covid19-us</bitbar.abouturl>
-curl "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=(Confirmed%20%3E%200)%20AND%20(Country_Region%3D%27US%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc%2CCountry_Region%20asc%2CProvince_State%20asc&outSR=102100&resultOffset=0&resultRecordCount=250&cacheHint=true" --silent > /tmp/covid19.json
+COVID_DATA_DIR="/Users/bert/code/COVID-19/"
+export PATH='/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH'
+cd $COVID_DATA_DIR
+git pull --rebase > /dev/null 2>&1
+cd "$COVID_DATA_DIR"csse_covid_19_data/csse_covid_19_daily_reports
+FILE=$(date +"%m-%d-%Y".csv)
+if [ ! -f "$FILE" ]; then
+    FILE=$(date -v -1d +"%m-%d-%Y".csv)
+fi
+xsv search -s 2 'US' "$FILE" | xsv select 4,6,5 | xsv stats | xsv select 3 | tail -n 3 > /tmp/covid19.txt
 echo -n "😷 "
-cat /tmp/covid19.json | /usr/local/bin/jq '[.features[].attributes.Confirmed] | add' | tr -d '\n'
+cat /tmp/covid19.txt | sed -n 1,1p | tr -d '\n'
 echo -n " 😳 "
-cat /tmp/covid19.json | /usr/local/bin/jq '[.features[].attributes.Recovered] | add' | tr -d '\n'
+cat /tmp/covid19.txt | sed -n 2,2p | tr -d '\n'
 echo -n " ⚰️ "
-cat /tmp/covid19.json | /usr/local/bin/jq '[.features[].attributes.Deaths] | add'
+cat /tmp/covid19.txt | sed -n 3,3p
